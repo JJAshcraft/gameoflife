07c290c0e62196d364348d16273d841768a67e7f
JJ Ashcraft <jj@jjashcraft.com>
2018-10-29 10:47:29 -0400

    Starts file/folder structure Implements initial class components for gameboard and gamesquare

---
+++ src/Gameboard/BoardContainer.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class BoardContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+BoardContainer.propTypes = {
+
+};
+
+export default BoardContainer;
